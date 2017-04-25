9d1cf4c42c7c70402742523433facd685d7230d7
Lin Dou <ldou@mlg.tv>
2017-04-24 20:24:44 -0400

    fix the object.

--- akamai-edgeAuth.js
+++ akamai-edgeAuth.js
@@ -1,70 +1,85 @@
-var cypsto = require('crypto');
-var _ = require('loadash');
+var crypto = require('crypto');
+var _ = require('lodash');
 
 var Akamai_EdgeAuth_Node = Object.create(null, {
 	config: {
-		algo: "SHA256",
-		ip: '',
-		start_time: Math.floor(new Date().getTime()/1000),
-		window: 300,
-		acl: '/*',
-		url: '',
-		session_id: '',
-		data: '',
-		salt: '',
-		key: 'aabbccddeeff00112233445566778899',
-		field_delimiter: '~'
+	    writable: true,
+	    value: {
+			algo: "sha256",
+			ip: '',
+			start_time: 0,
+			window: 300,
+			acl: '/*',
+			url: '',
+			session_id: '',
+			data: '',
+			salt: '',
+			key: 'aabbccddeeff00112233445566778899',
+			field_delimiter: '~'
+		}
 	}
 })
 
-Akamai_EdgeAuth_Node.generateToken = function(config) {
-	this.config = _.merge({}, this.config, conf);
+Akamai_EdgeAuth_Node.setConfig = function(config) {
+	this.config = _.merge({}, this.config, config);
+}
 
-  	var text = getIp() + getStartTime () + getExp() + getAcl() + getSessionId() + getData() + getSalt();
-
-	var signature = crypto.createHmac('sha256', this.config.key)
-		.update(text)
+Akamai_EdgeAuth_Node.generateToken = function() {
+  	var text = this.getIp() + this.getStartTime() + this.getExp() + this.getAcl() + this.getSessionId() + this.getData();
+  	var text_digest = text + this.getUrl() + this.getSalt()
+	var signature = crypto.createHmac(this.config.algo, this.config.key)
+		.update(text_digest)
 		.digest('hex');
 
 	return text + 'hmac=' + signature;
 }
 
-function getIp() {
-	if (Akamai_EdgeAuth_Node.config.ip)
-		return 'ip=' + Akamai_EdgeAuth_Node.config.ip + Akamai_EdgeAuth_Node.config.field_delimiter;
+Akamai_EdgeAuth_Node.getIp = function() {
+	if (this.config.ip)
+		return 'ip=' + this.config.ip + this.config.field_delimiter;
 	else
 		return '';
 }
 
-function getStartTime() {
-	return 'st=' + Akamai_EdgeAuth_Node.config.start_time + Akamai_EdgeAuth_Node.config.field_delimiter;
+Akamai_EdgeAuth_Node.getStartTime = function() {
+	if (this.config.start_time)
+		return 'st=' + this.config.start_time + this.config.field_delimiter;
+	else
+		return '';
 } 
 
-function getExp() {
-	return 'exp=' + (Math.floor(new Date().getTime()/1000) + Akamai_EdgeAuth_Node.config.window) + Akamai_EdgeAuth_Node.config.field_delimiter;
+Akamai_EdgeAuth_Node.getExp = function() {
+	return 'exp=' + (Math.floor(new Date().getTime()/1000) + this.config.window) + this.config.field_delimiter;
 }
 
-function getAcl() {
-	return 'acl=' + encodeURIComponent(Akamai_EdgeAuth_Node.config.acl) + Akamai_EdgeAuth_Node.config.field_delimiter;
+Akamai_EdgeAuth_Node.getAcl = function() {
+	return 'acl=' + escape(this.config.acl) + this.config.field_delimiter;
 }
 
-function getSessionId() {
-	if (Akamai_EdgeAuth_Node.config.session_id) 
-		return 'id=' + Akamai_EdgeAuth_Node.config.session_id + Akamai_EdgeAuth_Node.config.field_delimiter;
+Akamai_EdgeAuth_Node.getSessionId = function() {
+	if (this.config.session_id) 
+		return 'id=' + this.config.session_id + this.config.field_delimiter;
 	else
 		return '';
 }
 
-function getData() {
-	if (Akamai_EdgeAuth_Node.config.data)
-		return 'data=' + Akamai_EdgeAuth_Node.config.data + Akamai_EdgeAuth_Node.config.field_delimiter;
+Akamai_EdgeAuth_Node.getData = function() {
+	if (this.config.data)
+		return 'data=' + this.config.data + this.config.field_delimiter;
 	else
 		return '';
 }
 
-function getSalt() {
-	if  (Akamai_EdgeAuth_Node.config.salt)	
-		return 'salt=' + Akamai_EdgeAuth_Node.config.salt + Akamai_EdgeAuth_Node.config.field_delimiter;
+Akamai_EdgeAuth_Node.getUrl = function() {
+	if (this.config.url) 
+		return 'url=' + escape(this.config.url) + this.config.field_delimiter;
+	else
+		return '';
+}
+
+Akamai_EdgeAuth_Node.getSalt = function() {
+	if  (this.config.salt)	
+		return 'salt=' + this.config.salt + this.config.field_delimiter;
 	else
 		return '';
 }
